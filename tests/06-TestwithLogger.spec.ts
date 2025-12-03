7ae4dc69ed6278471908880b638791b895df7b8d
Luis Medina Coca <luismedinacoca@gmail.com>
2025-12-03 14:12:07 -0300

    section04-lecture034: Status Code Validadtor

--- tests/06-TestwithLogger.spec.ts
+++ tests/06-TestwithLogger.spec.ts
@@ -9,3 +9,111 @@ test("Test logger", async () => {
   const logs = logger.getRecentLogs();
   console.log(logs);
 });
+
+//***************
+
+let authToken: string;
+test.beforeAll("runs before all", async ({ api }) => {
+  console.log("\n\n\n🚀 LOGIN");
+  const tokenResponse = await api
+    .path("/users/login")
+    .body({ user: { email: "suspiros@test.com", password: "Test!001" } })
+    .postRequest(200);
+
+  authToken = "Token " + tokenResponse.user.token;
+  console.log("\n 🔐 authToken: ", authToken);
+});
+
+test("Second Test - GET Articles", async ({ api }) => {
+  const response = await api.path("/articles").params({ limit: 10, offset: 0 }).getRequest(200);
+  expect(response.articles.length).toBeLessThanOrEqual(10);
+  expect(response.articlesCount).toEqual(10);
+});
+
+test("Third Test - GET Tags", async ({ api }) => {
+  const response = await api.path("/tags").getRequest(200);
+  expect(response.tags.length).toBeLessThanOrEqual(10);
+  expect(response.tags[0]).toEqual("Test");
+});
+
+test("Create and Delete an Article", async ({ api }) => {
+  console.log("\n🚀 CREATE ARTICLE");
+  const newDate = Date.now();
+  const createArticleResponse = await api
+    .path("/articles")
+    .headers({ Authorization: authToken })
+    .body({
+      article: {
+        title: `Test TWO TEST ${newDate}`,
+        description: `Test TWO TEST ${newDate} - Description`,
+        body: "Test body",
+        tagList: ["suspiros", "payoneer"],
+      },
+    })
+    .postRequest(201);
+
+  const slugId = createArticleResponse.article.slug;
+  console.log("\n🚀 slugId: ", slugId);
+
+  console.log("\n🚀 GET ARTICLES");
+  const articleResponse = await api.path(`/articles`).headers({ Authorization: authToken }).getRequest(200);
+
+  expect(articleResponse.articles.find((a: any) => a.slug === slugId)).toBeDefined();
+
+  console.log("\n🚀 DELETE ARTICLE");
+  await api.path(`/articles/${slugId}`).headers({ Authorization: authToken }).deleteRequest(204);
+
+  console.log("\n🚀 GET ARTICLES - verify deleted");
+  const articleDoubleResponse = await api.path(`/articles`).headers({ Authorization: authToken }).getRequest(200);
+
+  // Verify that the article is not present in the response!
+  expect(articleDoubleResponse.articles.every((a: any) => a.slug !== slugId)).toBeTruthy();
+});
+
+test("Create, Update and Delete an Article", async ({ api }) => {
+  console.log("\n🚀 CREATE ARTICLE");
+  const newDate = Date.now();
+  const createArticleResponse = await api
+    .path("/articles")
+    .headers({ Authorization: authToken })
+    .body({
+      article: {
+        title: `Test TWO TEST ${newDate}`,
+        description: `Test TWO TEST ${newDate} - Description`,
+        body: "Test body",
+        tagList: [],
+      },
+    })
+    .postRequest(201);
+  const slugId = createArticleResponse.article.slug;
+  console.log("\n👍🏽 slugId: ", slugId);
+
+  console.log("\n🚀 GET ARTICLES");
+  const articleResponse = await api.path(`/articles`).headers({ Authorization: authToken }).getRequest(200);
+  expect(articleResponse.articles.find((a: any) => a.slug === slugId)).toBeDefined();
+
+  console.log("\n🚀 UPDATE ARTICLE");
+  const updateArticleResponse = await api
+    .path(`/articles/${slugId}`)
+    .headers({ Authorization: authToken })
+    .body({
+      article: {
+        title: `Test TWO UPDATED TEST ${newDate}`,
+        description: `Test TWO UPDATED TEST ${newDate} - Description`,
+        body: "******* Updated Test body *******",
+        tagList: ["suspiros", "payoneer"],
+        slug: slugId,
+      },
+    })
+    .putRequest(200);
+  const newSlugId = updateArticleResponse.article.slug;
+  console.log("\n👍🏽 newSlugId: ", newSlugId);
+
+  console.log("\n🚀 DELETE ARTICLE");
+  await api.path(`/articles/${newSlugId}`).headers({ Authorization: authToken }).deleteRequest(204);
+
+  console.log("\n🚀 GET ARTICLES - verify deleted");
+  const articleDoubleResponse = await api.path(`/articles`).headers({ Authorization: authToken }).getRequest(200);
+  // Verify that the article is not present in the response!
+  expect(articleDoubleResponse.articles.every((a: any) => a.slug !== newSlugId)).toBeTruthy();
+});
--- utils/request-handler.ts
+++ utils/request-handler.ts
@@ -1,18 +1,20 @@
 import { APIRequestContext, expect } from "@playwright/test";
+import { APILogger } from "./logger";
 
 export class RequestHandler {
   private request: APIRequestContext;
+  private logger: APILogger;
   private baseUrl: string;
   private defaultBaseUrl: string;
   private apiPath: string = "";
   private queryParams: object = {};
-  //private apiHeaders: object = {};
   private apiHeaders: Record<string, string> = {};
   private apiBody: object = {};
 
-  constructor(request: APIRequestContext, apiBaseUrl: string) {
+  constructor(request: APIRequestContext, apiBaseUrl: string, logger: APILogger) {
     this.request = request;
     this.defaultBaseUrl = apiBaseUrl;
+    this.logger = logger;
   }
 
   url(url: string) {
@@ -41,48 +43,104 @@ export class RequestHandler {
   }
 
   async getRequest(statusCode: number) {
+    // Get the URL
     const url = this.getUrl();
+
+    // Log the GET request
+    this.logger.logRequest("GET", url, this.apiHeaders, this.apiBody);
+
+    // Send the request
     const response = await this.request.get(url, {
       headers: this.apiHeaders,
     });
-    expect(response.status()).toEqual(statusCode);
+
+    // Obtain the actual status and response JSON
+    const actualStatus = response.status();
     const responseJSON = await response.json();
-    //console.log("\n🚀 responseJSON: ", responseJSON);
+
+    // Log the response
+    this.logger.logResponse(actualStatus, responseJSON);
+
+    // Assert the actual status is equal to the expected status
+    // ♻️ expect(actualStatus).toEqual(statusCode);
+    this.statusCodeValidator(actualStatus, statusCode, this.getRequest);
     return responseJSON;
   }
 
   async postRequest(statusCode: number) {
+    // Get the URL
     const url = this.getUrl();
+
+    // Log the POST request
+    this.logger.logRequest("POST", url, this.apiHeaders, this.apiBody);
+
+    // Send the request
     const response = await this.request.post(url, {
       headers: this.apiHeaders,
       data: this.apiBody,
     });
-    expect(response.status()).toEqual(statusCode);
+
+    // Obtain the actual status and response JSON
+    const actualStatus = response.status();
     const responseJSON = await response.json();
-    //console.log("\n🚀 responseJSON: ", responseJSON);
+
+    // Log the response
+    this.logger.logResponse(actualStatus, responseJSON);
+
+    // Assert the actual status is equal to the expected status
+    // ♻️ expect(actualStatus).toEqual(statusCode);
+    this.statusCodeValidator(actualStatus, statusCode, this.postRequest);
+
     return responseJSON;
   }
 
   async putRequest(statusCode: number) {
+    // Get the URL
     const url = this.getUrl();
+
+    // Log the PUT request
+    this.logger.logRequest("PUT", url, this.apiHeaders, this.apiBody);
+
+    // Send the PUT request
     const response = await this.request.put(url, {
       headers: this.apiHeaders,
       data: this.apiBody,
     });
-    expect(response.status()).toEqual(statusCode);
+
+    // Obtain the actual status and response JSON
+    const actualStatus = response.status();
     const responseJSON = await response.json();
-    //console.log("\n🚀 responseJSON: ", responseJSON);
+
+    // Log the response
+    this.logger.logResponse(actualStatus, responseJSON);
+
+    // Assert the actual status is equal to the expected status
+    // ♻️ expect(actualStatus).toEqual(statusCode);
+    this.statusCodeValidator(actualStatus, statusCode, this.putRequest);
+
     return responseJSON;
   }
 
   async deleteRequest(statusCode: number) {
     const url = this.getUrl();
+
+    // Log the DELETE request
+    this.logger.logRequest("DELETE", url, this.apiHeaders);
+
     const response = await this.request.delete(url, {
       headers: this.apiHeaders,
     });
-    expect(response.status()).toEqual(statusCode);
-    //const responseJSON = await response.json(); // Does not return JSON for DELETE requests
-    //console.log("\n🚀 responseJSON: ", responseJSON);
+
+    // Obtain the actual status
+    const actualStatus = response.status();
+
+    // Log the response
+    this.logger.logResponse(actualStatus);
+
+    // Assert the actual status is equal to the expected status
+    // ♻️ expect(actualStatus).toEqual(statusCode);
+    this.statusCodeValidator(actualStatus, statusCode, this.deleteRequest);
+
     return response;
   }
 
@@ -94,5 +152,15 @@ export class RequestHandler {
     }
     //console.log("\n🚀 url: ", url.toString(), "\n");
     return url.toString();
+  }
+
+  // Private method to validate the status code in "expect(actualStatus).toEqual(statusCode);"
+  private statusCodeValidator(actualStatus: number, expectedStatus: number, callingMethod: Function) {
+    if (actualStatus !== expectedStatus) {
+      const logs = this.logger.getRecentLogs();
+      const error = new Error(`Expected status ${expectedStatus} but got ${actualStatus}\n\nRecent API Activity: \n${logs}`);
+      Error.captureStackTrace(error, callingMethod);
+      throw error;
+    }
   }
 }
